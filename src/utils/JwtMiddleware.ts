aff4bfe55dcc8ca015c6258230e5d7c64ab5abe3
leo <leo_9_t@hotmail.com>
2020-09-20 00:46:58 -0400

    ADDED debug

--- src/utils/JwtMiddleware.ts
+++ src/utils/JwtMiddleware.ts
@@ -5,6 +5,7 @@ import { getDbFor } from '../utils/DB';
 export default () => {
     return (req: express.Request, res: express.Response, n: express.NextFunction) => {
         const ip = req.headers['x-real-ip'] || req.connection.remoteAddress
+        console.log(ip)
         getDbFor("grcgds_hannk")?.select().where('ip', ip).table("whitelist_ip")
         .then((r) => {
             if (r.length != 0) {
