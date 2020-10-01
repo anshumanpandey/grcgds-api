09bbc350a6fb28aa0dfc5c4aba1f94e819742469
leo <leo_9_t@hotmail.com>
2020-10-01 18:46:28 -0400

    ADDED check

--- src/carSearchUtils/MergeResults.ts
+++ src/carSearchUtils/MergeResults.ts
@@ -1,6 +1,8 @@
 export default (RcResults: any, ...rest: any[]) => {
     rest.forEach(r => {
-        RcResults.OTA_VehAvailRateRS.VehVendorAvails[0].VehVendorAvail[0].VehAvails[0].VehAvail.push(...r)
+        if (r.lenght > 0) {
+            RcResults.OTA_VehAvailRateRS.VehVendorAvails[0].VehVendorAvail[0].VehAvails[0].VehAvail.push(...r)
+        }
     })
 
     return RcResults
