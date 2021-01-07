bbdc8171f8554366ebdf8594bd48e6244ed7e816
leo <leo_9_t@hotmail.com>
2021-01-07 14:17:19 -0400

    FIX on val

--- src/utils/JwtMiddleware.ts
+++ src/utils/JwtMiddleware.ts
@@ -32,6 +32,7 @@ export default () => {
                         })
                 } else {
                     n();
+                    return [1]
                 }
             })
             .then((r) => {
