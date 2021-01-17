4fdfe4c6b0bc83591c3f5975689a501f106b1e6a
leo <leo_9_t@hotmail.com>
2021-01-17 03:09:25 -0400

    FIX on id

--- src/controllers/booking.controller.ts
+++ src/controllers/booking.controller.ts
@@ -14,7 +14,7 @@ const allSettled = require('promise.allsettled');
 
 const schema = {
     "$schema": "http://json-schema.org/draft-07/schema",
-    "$id": "http://example.com/example.json",
+    "$id": "OTA_VehResRQReq",
     "type": "object",
     "title": "The root schema",
     "description": "The root schema comprises the entire JSON document.",
