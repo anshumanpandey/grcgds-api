a125c89a3e7dc85c12c94089c888194e664c09dc
Anshuman <Anshuman>
2021-03-11 16:54:22 -0400

    FIX on filteringh

--- src/utils/FilterBrandsForClient.ts
+++ src/utils/FilterBrandsForClient.ts
@@ -1,14 +1,15 @@
 import { DB } from '../utils/DB';
 
 export const FilterBrandsForClient = async (grcCode: string) => {
-    const blockedBrands = (await DB?.select().from("BlockedBrands").where({ grcCode })) || []
+    const blockedBrands = await DB?.select().from("BlockedBrands") || []
 
     return (result: any) => {
         if (blockedBrands.length == 0) return true
 
-        const brand = result.VehAvailCore[0].$.Supplier_Name.toLowerCase().replace(/\s/g,'')
+        const clientName = result.VehAvailCore[0].$.Supplier_Name.toLowerCase().replace(/\s/g,'')
+        const brand = result.VehAvailCore[0].Vehicle[0].$.Brand.toLowerCase().replace(/\s/g,'')
         return !blockedBrands
-            .filter(i => i.brandName.toLowerCase().replace(/\s/g,'') == brand)
+            .filter(i => i.supplierId.toLowerCase().replace(/\s/g,'') == clientName)
             .map(r => r.brandName.toLowerCase().replace(/\s/g,'')).includes(brand)
     }
 }
