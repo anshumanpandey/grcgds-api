421346fa4566cd0d3be4cb8489093ab43dc1faef
Anshuman <Anshuman>
2021-03-16 09:57:43 -0400

    FIC on optional paramtet

--- src/utils/FilterBrandsForClient.ts
+++ src/utils/FilterBrandsForClient.ts
@@ -7,7 +7,7 @@ export const FilterBrandsForClient = async (grcCode: string) => {
         if (blockedBrands.length == 0) return true
 
         const clientName = result.VehAvailCore[0].$.Supplier_Name.toLowerCase().replace(/\s/g,'')
-        const brand = result.VehAvailCore[0].Vehicle[0].$.Brand.toLowerCase().replace(/\s/g,'')
+        const brand = result.VehAvailCore[0].Vehicle[0].$?.Brand?.toLowerCase().replace(/\s/g,'')
         return !blockedBrands
             .filter(i => i.supplierId.toLowerCase().replace(/\s/g,'') == clientName)
             .map(r => r.brandName.toLowerCase().replace(/\s/g,'')).includes(brand)
