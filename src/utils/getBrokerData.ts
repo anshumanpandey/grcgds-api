4a0601e651931efe23d81f3518dcf0a21b659eac
Anshuman <Anshuman>
2021-07-08 13:29:37 -0400

    FIX on query

--- src/utils/getBrokerData.ts
+++ src/utils/getBrokerData.ts
@@ -8,13 +8,16 @@ export type BrokerData = {
 type Params = { brokerId?:  string | number, locationCode?: string | number, brokerAccountCode?: string | number }
 export const getBrokerData = async (p: Params): Promise<BrokerData> => {
     const query = DB?.select({
-        internalCode: 'internal_code',
-        clientId: 'clientId',
+        internalCode: 'client_broker_locations_accountype.internal_code',
+        clientId: 'client_broker_locations_accountype.clientId',
     })
         .from("client_broker_locations_accountype")
-        .groupBy('account_code')
+        .groupBy('client_broker_locations_accountype.account_code')
     if (p.brokerAccountCode) query?.where("client_broker_locations_accountype.account_code", p.brokerAccountCode)
-    if (p.locationCode) query?.where("client_broker_locations_accountype.brokerInternalCode", p.locationCode)
+    if (p.locationCode) {
+        query?.innerJoin('companies_locations', 'client_broker_locations_accountype.locationId', 'companies_locations.id')
+        query?.where("companies_locations.GRCGDSlocatincode", p.locationCode)
+    }
     if (p.brokerId) query?.where("client_broker_locations_accountype.brokerId", p.brokerId)
 
     const r = await query;
